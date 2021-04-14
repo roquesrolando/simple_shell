ff9d0a666318c62b34374975ce203d2f83cac5fe
roquesrolando <roquesrolando@hotmail.com>
2021-04-14 23:37:08 +0000

    betty error

--- execute.c
+++ execute.c
@@ -4,6 +4,7 @@
  * executeCommand - function exectues command
  * @fullPath: name of binary file that will execute
  * @commandLineArgs: command along with options
+ * Return: returns 0 if success
  */
 
 int executeCommand(char *fullPath, char **commandLineArgs)
